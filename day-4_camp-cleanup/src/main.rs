d69145b08d6afd239955e5355ccca238e2496415
Daniel Bunte <daniel.bunte.84+github@gmail.com>
2022-12-04 11:09:41 +0100

    Day-4: Round two

--- day-4_camp-cleanup/src/main.rs
+++ day-4_camp-cleanup/src/main.rs
@@ -2,8 +2,18 @@ use std::ops::Range;
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let score = get_total_number_of_fully_contained_ranges(input);
-    println!("The total sum of overlapping sections is {score}");
+    let score = get_total_number_of_fully_contained_ranges(input.clone());
+    println!("The total sum of fully overlapping sections is {score}");
+
+    let score_round_two = get_total_number_of_overlapping_ranges(input);
+    println!("The total sum of partially overlapping sections is {score_round_two}");
+}
+
+fn get_total_number_of_overlapping_ranges(input: String) -> usize {
+    input.lines().into_iter().map(|it| it.trim())
+        .map(|it| get_pair_of_sections(it.to_string()))
+        .filter(|it| check_overlap(&it[0], &it[1]))
+        .count()
 }
 
 fn get_total_number_of_fully_contained_ranges(input: String) -> usize {
@@ -27,10 +37,14 @@ fn fully_contained_in_ranges(a: &Range<usize>, b: &Range<usize>) -> bool {
         b.end <= a.end && b.start >= a.start
 }
 
+fn check_overlap(a: &Range<usize>, b: &Range<usize>) -> bool {
+    a.start <= b.start && a.end >= b.start || b.start <= a.start && b.end >= a.start
+}
+
 #[cfg(test)]
 mod tests {
     use std::{assert_eq, vec};
-    use crate::{fully_contained_in_ranges, get_pair_of_sections, get_total_number_of_fully_contained_ranges};
+    use super::*;
 
     #[test]
     fn gets_pairs_of_sections() {
@@ -71,5 +85,47 @@ mod tests {
 
         // then: we get the correct pairs of sections
         assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn checks_if_ranges_overlap() {
+        // given: two ranges
+        let ranges = vec![5..7, 7..9];
+
+        // when: we check if they overlap
+        let result = check_overlap(&ranges[0], &ranges[1]);
+
+        // then: we get the correct result
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn checks_if_ranges_do_not_overlap() {
+        // given: two ranges
+        let ranges = vec![2..4, 6..8];
+
+        // when: we check if they overlap
+        let result = check_overlap(&ranges[0], &ranges[1]);
+
+        // then: we get the correct result
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn gets_total_number_of_overlapping_ranges() {
+        // given: a string of section groups
+        let input = r#"2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+            9-9,4-6"#.to_string();
+
+        // when: get_total_number_of_overlapping_ranges is called
+        let result = get_total_number_of_overlapping_ranges(input);
+
+        // then: we get the correct pairs of sections
+        assert_eq!(result, 4);
     }
 }
