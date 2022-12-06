3331db7d2c1f8f5350c911c7c964139d6d823913
Daniel Bunte <daniel.bunte.84+github@gmail.com>
2022-12-07 00:11:24 +0100

    Day-6: Round two, unfinished. It's too late today, so I'll stop here. What needs to be done: compare each token

--- day-6_tuning-trouble/src/main.rs
+++ day-6_tuning-trouble/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::LinkedList;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let marker = get_start_of_packet_marker(input.as_bytes());
     println!("First packet marker after: {marker}");
 
-    let marker = get_start_of_message_marker(input.as_bytes());
+    let marker = get_start_of_message_marker(input.as_bytes(),14);
     println!("First message marker after: {marker}");
 }
 
@@ -28,23 +30,18 @@ fn get_start_of_packet_marker(input_stream: &[u8]) -> usize {
     0
 }
 
-fn get_start_of_message_marker(input_stream: &[u8]) -> usize {
-    let mut unique_tokens_found = 0u128;
+fn get_start_of_message_marker(input_stream: &[u8], min_length: usize) -> usize {
+    let mut unique_tokens_found = LinkedList::new();
     for (index, byte) in input_stream.iter().enumerate() {
-        unique_tokens_found <<= 8;
-        unique_tokens_found |= *byte as u128;
-        let as_bits = format!("{:#X}", unique_tokens_found);
-        if index > 3 {
-            if ((unique_tokens_found & 0xFF00000000000000000000000000) >> 24) != *byte as u128
-                && ((unique_tokens_found & 0x00FF0000) >> 16) != *byte as u128
-                && ((unique_tokens_found & 0x0000FF00) >> 8) != *byte as u128
-                && ((unique_tokens_found & 0xFF000000) >> 24) != ((unique_tokens_found & 0x0000FF00) >> 8)
-                && ((unique_tokens_found & 0x00FF0000) >> 16) != ((unique_tokens_found & 0x0000FF00) >> 8)
-                && ((unique_tokens_found & 0xFF000000) >> 24) != ((unique_tokens_found & 0x00FF0000) >> 16)
-            {
+        if unique_tokens_found.len() >= min_length {
+            unique_tokens_found.pop_front();
+            let mut unique = unique_tokens_found.iter().map(|it|*it).collect::<Vec<u8>>();
+            unique.dedup();
+            if !unique_tokens_found.contains(byte) && unique.len() >= min_length-1 {
                 return index + 1;
             }
         }
+        unique_tokens_found.push_back(*byte);
     }
     0
 }
@@ -80,7 +77,7 @@ mod tests {
         ];
 
         // when: the get_marker function is invoked
-        let result = input.iter().map(|it| get_start_of_message_marker(it.0.as_bytes())).collect::<Vec<usize>>();
+        let result = input.iter().map(|it| get_start_of_message_marker(it.0.as_bytes(), 14)).collect::<Vec<usize>>();
 
         // then: the first marker with x (configurable) unique tokens is returned
         assert_eq!(result, input.iter().map(|it| it.1).collect::<Vec<usize>>());
